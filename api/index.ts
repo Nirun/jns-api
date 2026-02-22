55927e753a2a1b260ffcdc24c362d796ec52a1aa
Nirun junvityee <finch255@hotmail.com>
2026-02-22 15:38:24 +0700

    Add SwaggerModule import to index.ts

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from '../src/app.module';
 import express from 'express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const server = express();
 
