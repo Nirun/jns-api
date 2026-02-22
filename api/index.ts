b4463dd99bdb1c147690de34f2a6c9d4161cc416
Nirun junvityee <finch255@hotmail.com>
2026-02-22 15:33:07 +0700

    Integrate Swagger documentation setup in index.ts

--- api/index.ts
+++ api/index.ts
@@ -17,6 +17,15 @@ export const createServer = async (expressInstance) => {
   
   // If you use a global prefix in main.ts, add it here too
   // app.setGlobalPrefix('api'); 
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document, {
+    // These links ensure the UI loads even if Vercel doesn't serve the local CSS/JS
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
 
   await app.init();
 };
@@ -26,12 +35,4 @@ export default async (req: any, res: any) => {
   return server(req, res);
 };
 
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api/docs', app, document, {
-  // These links ensure the UI loads even if Vercel doesn't serve the local CSS/JS
-  customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-  customJs: [
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
-  ],
-});
+
